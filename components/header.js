0eaa56f719a0723a4b3f57000ed04e731abc4a2d
Simon Gagnon <simon.gagnonr@gmail.com>
2021-01-31 16:01:12 -0500

    change me image to be more light

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <div className="flex items-center">
-        <img src="/assets/blog/site/me.png" className="w-16 h-16 rounded-full mr-4" alt="Simon Gagnon" />
+        <img src="/assets/blog/site/me.jpg" className="w-16 h-16 rounded-full mr-4" alt="Simon Gagnon" />
         <div className="text-xxl font-bold"><Link href="/">Simon</Link></div>
       </div>
     </h2>
