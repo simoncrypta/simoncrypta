56381bfa5ef59d5e31690136c7f2afd60b0d8a6e
Simon Gagnon <simon.gagnonr@gmail.com>
2021-01-31 15:01:52 -0500

    Remove footer

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,4 @@
 import Alert from '../components/alert'
-import Footer from '../components/footer'
 import Meta from '../components/meta'
 
 export default function Layout({ preview, children }) {
@@ -10,7 +9,6 @@ export default function Layout({ preview, children }) {
         <Alert preview={preview} />
         <main>{children}</main>
       </div>
-      <Footer />
     </>
   )
 }
