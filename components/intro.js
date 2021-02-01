bcd24bf829e0ec6dbdd950e45f15e409d65d8253
Simon Gagnon <simon.gagnonr@gmail.com>
2021-02-01 18:44:36 -0500

    My intro text

--- components/intro.js
+++ components/intro.js
@@ -3,9 +3,27 @@
 export default function Intro() {
   return (
     <section className="mt-16 mb-16 md:mb-12">
-      <h2 className="text-lg mt-5 md:pl-8">
-        A statically generated blog 
-      </h2>
+      <p className="text-lg mt-5 md:pl-8">
+        Hello, my name is Simon.
+        <br />
+        I'm a full-stack Javascript developer, currently in love with <a className="text-green-600" href="https://jamstack.org">JAMstack</a> and Serverless.
+      </p>
+      <p className="text-lg mt-5 md:pl-8">
+        I build cool stuff for <a className="text-green-600"href="https://lemacurbain.com">Le Mac Urbain</a>.
+        <br />
+        Also, I have a big project name <a className="text-green-600"href="https://raccoon.services">Raccoon</a>.
+      </p>
+
+      <p className="text-lg mt-5 md:pl-8">
+        You can follow me on <a className="text-green-600"href="https://twitter.com/SimonCrypta">Twitter</a>,<br />
+        Connect with me on <a className="text-green-600"href="https://www.linkedin.com/in/gagnonsim/">Linkedin</a>,<br />
+        or check my <a className="text-green-600"href="https://github.com/simoncrypta">Github</a>.<br />
+      </p>
+
+      <p className="text-lg mt-5 md:pl-8">
+        Feel free to chat with me 👋
+      </p>
+
     </section>
   )
 }
